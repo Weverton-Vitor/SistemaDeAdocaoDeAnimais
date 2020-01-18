e31ee4757b149e448347a633b494409be6599fe7
Weverton Vitor <wevertonvitor01@gmail.com>
2020-01-18 07:42:19 -0300

    Adição de um endereço fictício na página 'sobre'

--- resources/views/Site/sobre.blade.php
+++ resources/views/Site/sobre.blade.php
@@ -3,7 +3,14 @@
 <div class="container-fluid text-center container-contato-sobre">    
     <div class="sombra">
         <h1 style="margin-bottom: 20px"> {{$cvHeaderPage}} </h1>
-        <p style="font-size: 30px">Somos uma ONG disposta a arranjar um novo lar para animais abandonados</p>
+        <p style="font-size: 30px">
+            Somos uma ONG disposta a arranjar um novo lar para animais abandonados.<br>    
+            Estamos localizado no seguinte endereco:<br>
+            Cidade: xxxxxxx xxxxxxx<br>
+            Bairro: xxxxxxx xxxxxxxx<br>
+            Rua: xxxxxxx xxxxxxxxx<br> 
+            Nº: xxxxxxxxxxxxxxxxxx            
+        </p>        
     </div>
 </div>
 @endsection
